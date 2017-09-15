acf0be1e84f69881d23ed7aecfccb8891e2057ed
Christian <cristian.bongiorno@unipa.it>
2017-09-15 11:05:35 +0200

    bug

--- hclustval/MainLib.py
+++ hclustval/MainLib.py
@@ -198,7 +198,7 @@ def HValidate(LV,Rb,method,alpha=0.05):
 
 	return L
 	
-def HclustVal(X,alpha=0.05,Nt=1000,method='average',boottype='standard',P=None,M=None):
+def HclustVal(X,alpha=0.05,Nt=1000,method='average',boottype='standard',pattern=None,M=None):
 	
 	if pattern!=None and M==None:
 		print "If you specify the pattern, you must specify the dataseries lenght (M) too"
